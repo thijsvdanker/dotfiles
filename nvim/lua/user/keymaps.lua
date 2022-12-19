dac74aee3779479e11a2bf9407bcb9fabb5c0037
Thijs van den Anker <thijs@bitsnbolts.nl>
2022-12-19 11:08:00 +0100

    Add keymaps

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,42 @@
+-- Space is my leader key.
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- Reselect visual selection after indenting.
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Maintain the cursor position position when yanking a visual selection.
+vim.keymap.set('v', 'y', 'myy`y')
+vim.keymap.set('v', 'Y', 'myY`y')
+
+-- Disable annoying command line typo.
+vim.keymap.set('n', 'q:', ':q')
+
+-- Paste replace visual selection without copying it.
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Easy insertion of a trailing ; or , from insert mode.
+vim.keymap.set('i', ';;', '<Esc>A;')
+vim.keymap.set('i', ',,', '<Esc>A,')
+
+-- Quickly clear search highlighting.
+vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>')
+
+-- Open the current file in the default program (on Mac this should be just 'open').
+vim.keymap.set('n', '<Leader>x', ':!xdg-open %<CR><CR>')
+
+-- Move lines up and down. (hrmm command j works, command k does not)
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
+vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
+vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv=gv")
+
